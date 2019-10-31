6140b066524be0d8464430afc6dd03b889ad17b9
yanxxjin <yjkris@hotmail.com>
2019-10-31 03:59:35 +0100

    Update title.js

--- js/title.js
+++ js/title.js
@@ -2,11 +2,11 @@ var OriginTitile = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function() {
     if (document.hidden) {
-        document.title = '喂喂？';
+        document.title = 'hello?';
         clearTimeout(titleTime);
     }
     else {
-        document.title = '(*´∇｀*)' + OriginTitile;
+        document.title = '(*´∇｀*)'  + OriginTitile;
         titleTime = setTimeout(function() {
             document.title = OriginTitile;
         }, 2000);
